e267383a5f673779a56b9dd45638c44d3833965e
devid-charli <mustakali444888@gmail.com>
2024-06-27 16:38:18 +0600

    String variable

--- 26style.js
+++ 26style.js
@@ -1,1 +1,10 @@
 console.log("her power project");
+
+var myVillage =`prity`
+console.log(typeof myVillage)
+
+var myFather ="Ashik"
+console.log(myFather)
+
+var myMother ='Pervin'
+console.log(myMother)
